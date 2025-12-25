704751204e56c13c981937f37c8b78347f1ce6b8
Codrix1 <WorkCodrix@gmail.com>
2025-12-25 22:12:18 +0200

    reload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import ProtectedRoute from "./components/ProtectedRoute";
 import NotFound from "./pages/NotFound";
+import ReloadRedirect from "./components/ReloadRedirect";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,6 +27,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ReloadRedirect />
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route
